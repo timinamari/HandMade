d2da7076574c0c0e172d067f197bd0591e105ab7
timinamari <86426726+timinamari@users.noreply.github.com>
2022-04-06 19:10:14 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$('.slider_wrapper').slick({
+		prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/prevArrow.svg"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="img/icons/nextArrow.svg"></button>'
+	});
+ });
